1b7b89f4aa4dae98dda559ee9370e37b8289bda4
Daniel Poss <dpatposs@msn.com>
2022-05-19 17:37:06 -0500

    - Adding in barebones lambda with Jest and CFT template

--- lambda/code/src/index.ts
+++ lambda/code/src/index.ts
@@ -2,11 +2,11 @@ import {APIGatewayEventRequestContext, APIGatewayProxyResult, SNSEvent} from "aw
 
 export function handler(event: SNSEvent, context?: APIGatewayEventRequestContext): Promise<APIGatewayProxyResult> {
     console.log(JSON.stringify(event));
-    return new Promise<APIGatewayProxyResult>(() => {
-        return {
+    return new Promise<APIGatewayProxyResult>((resolve) => {
+        resolve({
             statusCode: 200,
             body: `Successfully posted logs`
-        }
+        })
     });
 }
 
