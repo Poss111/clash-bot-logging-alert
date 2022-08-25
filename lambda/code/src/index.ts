b72845a7f104bf2302a464284c76ed0f0fff6f8f
Poss111 <dpatposs@msn.com>
2022-08-24 23:36:49 -0500

    - Parsing new error logs from Clash Bot Service

--- lambda/code/src/index.ts
+++ lambda/code/src/index.ts
@@ -1,46 +1,60 @@
 import {APIGatewayEventRequestContext, APIGatewayProxyResult} from "aws-lambda";
 import {CloudWatchLogsEvent} from "aws-lambda/trigger/cloudwatch-logs";
 import * as zlib from "zlib";
-import {buildPayload} from "./template-builder";
+import {buildGenericErrorPayload, buildRequestErrorPayload} from "./template-builder";
 import axios from "axios";
+
+export function buildAndSendDiscordPayload(loggingOutput: any) {
+    let message = JSON.parse(loggingOutput.logEvents[0].message);
+    console.log("Event Data:", JSON.stringify(JSON.parse(loggingOutput.logEvents[0].message), null, 2));
+    let discordWebhookPayload = message.req ?
+        buildRequestErrorPayload(message.req.id, message.req.method, message.req.url, JSON.stringify(message.req.params), message.msg)
+        : buildGenericErrorPayload(message.class, message.method, message.msg, message.error);
+    console.log("Payload:", JSON.stringify(discordWebhookPayload, null, 2));
+    return discordWebhookPayload;
+}
+
+function failedToParseLogMessage(e: Error) {
+    return {
+        statusCode: 500,
+        body: JSON.stringify({
+            attachments: [],
+            content: undefined,
+            embeds: [{
+                color: 16580608,
+                fields: [
+                    {name: "Failed", value: "Unable to parse log. Check Lambda."},
+                    {name: "Exception", value: JSON.stringify(e)}
+                ],
+                title: "Clash Bot - Error Log"
+            }]
+        })
+    };
+}
 
 export function handler(event: CloudWatchLogsEvent, context?: APIGatewayEventRequestContext): Promise<APIGatewayProxyResult> {
     const payload = Buffer.from(event.awslogs.data, 'base64');
-    let loggingOutput;
     return new Promise<APIGatewayProxyResult>((resolve, reject) => {
         zlib.gunzip(payload, function (e, result) {
             if (e) {
-                reject({
-                    statusCode: 500,
-                    body: JSON.stringify({
-                        attachments: [],
-                        content: undefined,
-                        embeds: [{
-                            color: 16580608,
-                            fields: [
-                                {name: "Failed", value: "Unable to parse log. Check Lambda."},
-                                {name: "Exception", value: JSON.stringify(e)}
-                            ],
-                            title: "Clash Bot - Error Log"
-                        }]
-                    })
-                });
+                reject(failedToParseLogMessage(e));
             } else {
-                loggingOutput = JSON.parse(result.toString());
-                let message = JSON.parse(loggingOutput.logEvents[0].message);
-                console.log("Event Data:", JSON.stringify(JSON.parse(loggingOutput.logEvents[0].message), null, 2));
-                let discordWebhookPayload = buildPayload(message.req.id, message.req.method, message.req.url, JSON.stringify(message.req.params), message.msg);
-                console.log("Payload:", JSON.stringify(discordWebhookPayload, null, 2));
+                let payload = {};
+                try {
+                    payload = buildAndSendDiscordPayload(JSON.parse(result.toString()));
+                } catch(err: any) {
+                    payload = failedToParseLogMessage(err);
+                }
                 axios({
                     method: "POST",
                     url: process.env.DISCORD_WEBHOOK_URL,
-                    data: discordWebhookPayload,
+                    data: payload,
                     timeout: 10000
                 }).then((res) => {
                     console.log(res);
                     resolve({
                         statusCode: 200,
-                        body: JSON.stringify(discordWebhookPayload),
+                        body: JSON.stringify(payload),
                     })
                 }).catch(err => {
                     console.error(err);
--- lambda/code/src/test/template-builder.spec.ts
+++ lambda/code/src/test/template-builder.spec.ts
@@ -1,25 +1,68 @@
 import {DiscordWebhookPayload} from "../interface/DiscordWebhookPayload";
-import {buildPayload} from "../template-builder";
+import {buildGenericErrorPayload, buildRequestErrorPayload} from "../template-builder";
 
 describe('Template Builder', () => {
-    test('(Successful) - When template builder is given with the log details it should be built successfully.', () => {
-        const builtPayload: DiscordWebhookPayload = buildPayload(1, "GET", "/dne", "something=2", "Failed to find path.");
-        expect(builtPayload).toBeTruthy();
-        expect(builtPayload.embeds).toHaveLength(1);
-        expect(builtPayload.embeds[0].title).toEqual("Clash Bot - Error Log");
-        expect(builtPayload.embeds[0].color).toEqual(16580608);
-        expect(builtPayload.embeds[0].fields).toHaveLength(5);
-        expect(builtPayload.embeds[0].fields[0].name).toEqual("ID");
-        expect(builtPayload.embeds[0].fields[0].value).toEqual("1");
-        expect(builtPayload.embeds[0].fields[1].name).toEqual("Request Type");
-        expect(builtPayload.embeds[0].fields[1].value).toEqual("GET");
-        expect(builtPayload.embeds[0].fields[2].name).toEqual("Request Path");
-        expect(builtPayload.embeds[0].fields[2].value).toEqual("/dne");
-        expect(builtPayload.embeds[0].fields[3].name).toEqual("Parameters");
-        expect(builtPayload.embeds[0].fields[3].value).toEqual("something=2");
-        expect(builtPayload.embeds[0].fields[4].name).toEqual("Message");
-        expect(builtPayload.embeds[0].fields[4].value).toEqual("Failed to find path.");
-        expect(builtPayload.attachments).toHaveLength(0);
-        expect(builtPayload.content).toBeFalsy();
+
+    describe('Request Error Handler', () => {
+        test('(Successful) - When template builder is given with the log details it should be built successfully.', () => {
+            const builtPayload: DiscordWebhookPayload = buildRequestErrorPayload(1, "GET", "/dne", "something=2", "Failed to find path.");
+            expect(builtPayload).toBeTruthy();
+            expect(builtPayload.embeds).toHaveLength(1);
+            expect(builtPayload.embeds[0].title).toEqual("Clash Bot - Request Error Log");
+            expect(builtPayload.embeds[0].color).toEqual(16580608);
+            expect(builtPayload.embeds[0].fields).toHaveLength(5);
+            expect(builtPayload.embeds[0].fields[0].name).toEqual("ID");
+            expect(builtPayload.embeds[0].fields[0].value).toEqual("1");
+            expect(builtPayload.embeds[0].fields[1].name).toEqual("Request Type");
+            expect(builtPayload.embeds[0].fields[1].value).toEqual("GET");
+            expect(builtPayload.embeds[0].fields[2].name).toEqual("Request Path");
+            expect(builtPayload.embeds[0].fields[2].value).toEqual("/dne");
+            expect(builtPayload.embeds[0].fields[3].name).toEqual("Parameters");
+            expect(builtPayload.embeds[0].fields[3].value).toEqual("something=2");
+            expect(builtPayload.embeds[0].fields[4].name).toEqual("Message");
+            expect(builtPayload.embeds[0].fields[4].value).toEqual("Failed to find path.");
+            expect(builtPayload.attachments).toHaveLength(0);
+            expect(builtPayload.content).toBeFalsy();
+        })
+    })
+
+    describe('Generic Error Handler', () => {
+        test('(Successful) - When template builder is given with the log details from a generic error message it should be built successfully.', () => {
+            const builtPayload: DiscordWebhookPayload = buildGenericErrorPayload("TeamsService", "getTeams", "Failed to parse.", undefined);
+            expect(builtPayload).toBeTruthy();
+            expect(builtPayload.embeds).toHaveLength(1);
+            expect(builtPayload.embeds[0].title).toEqual("Clash Bot - Generic Error Log");
+            expect(builtPayload.embeds[0].color).toEqual(16580608);
+            expect(builtPayload.embeds[0].fields).toHaveLength(3);
+            expect(builtPayload.embeds[0].fields[0].name).toEqual("Class");
+            expect(builtPayload.embeds[0].fields[0].value).toEqual("TeamsService");
+            expect(builtPayload.embeds[0].fields[1].name).toEqual("Method");
+            expect(builtPayload.embeds[0].fields[1].value).toEqual("getTeams");
+            expect(builtPayload.embeds[0].fields[2].name).toEqual("Message");
+            expect(builtPayload.embeds[0].fields[2].value).toEqual("Failed to parse.");
+            expect(builtPayload.attachments).toHaveLength(0);
+            expect(builtPayload.content).toBeFalsy();
+        })
+
+        test('(Successful) - When template builder is given with the log details from a generic error message with a stack it should be built successfully.', () => {
+            const builtPayload: DiscordWebhookPayload = buildGenericErrorPayload("TeamsService", "getTeams", "Failed to parse.", { message: 'Failed to parse', stack: 'Stacky'});
+            expect(builtPayload).toBeTruthy();
+            expect(builtPayload.embeds).toHaveLength(1);
+            expect(builtPayload.embeds[0].title).toEqual("Clash Bot - Generic Error Log");
+            expect(builtPayload.embeds[0].color).toEqual(16580608);
+            expect(builtPayload.embeds[0].fields).toHaveLength(5);
+            expect(builtPayload.embeds[0].fields[0].name).toEqual("Class");
+            expect(builtPayload.embeds[0].fields[0].value).toEqual("TeamsService");
+            expect(builtPayload.embeds[0].fields[1].name).toEqual("Method");
+            expect(builtPayload.embeds[0].fields[1].value).toEqual("getTeams");
+            expect(builtPayload.embeds[0].fields[2].name).toEqual("Error Message");
+            expect(builtPayload.embeds[0].fields[2].value).toEqual("Failed to parse");
+            expect(builtPayload.embeds[0].fields[3].name).toEqual("Stack");
+            expect(builtPayload.embeds[0].fields[3].value).toEqual("Stacky");
+            expect(builtPayload.embeds[0].fields[4].name).toEqual("Message");
+            expect(builtPayload.embeds[0].fields[4].value).toEqual("Failed to parse.");
+            expect(builtPayload.attachments).toHaveLength(0);
+            expect(builtPayload.content).toBeFalsy();
+        })
     })
 })
